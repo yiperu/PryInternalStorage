455dba21fb087b9f72f7d3050321c349f906ffc3
Henry AT <yiperu@gmail.com>
2014-03-18 00:12:49 -0300

    Creacion de la funcinoes necesarias para la escritura y lectura de los archivos

--- src/com/apps4s/pryinternalstorage/MainActivity.java
+++ src/com/apps4s/pryinternalstorage/MainActivity.java
@@ -1,5 +1,11 @@
 package com.apps4s.pryinternalstorage;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
@@ -10,6 +16,9 @@ public class MainActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		
+		//createInternalStorage();
+		readInternalStorage();
 	}
 
 	@Override
@@ -19,4 +28,39 @@ public class MainActivity extends Activity {
 		return true;
 	}
 
+	// Creamos la funcion que creara y guardara datos
+	private void createInternalStorage(){
+		try {
+			OutputStreamWriter osw = new OutputStreamWriter(openFileOutput("elArchivo.txt",0));
+			osw.write("Contenido de Prueva");
+			osw.flush();
+			osw.close();
+		} catch (FileNotFoundException e) {
+					e.printStackTrace();
+		} catch (IOException e) {
+				e.printStackTrace();
+		}
+	}
+	// Crearemos la funcion que nos permite leer los datos guardados:
+	private void readInternalStorage(){
+		try {
+			BufferedReader br = new BufferedReader(new InputStreamReader(openFileInput("elArchivo.txt")));
+			String str = br.readLine();
+			System.out.println(str);
+			br.close();
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
+		
+		
+	}
+	
+	
+	
+	
+	
+	
 }
